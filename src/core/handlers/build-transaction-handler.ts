c7f9379bbaed70c6753f6a1d2f7a05a413b678ce
marco.ajunior <marco.ajunior@luizalabs.com>
2025-12-26 10:35:06 -0300

    refactor: remove OpenAI categorization from the transaction handler chain.

--- src/core/handlers/build-transaction-handler.ts
+++ src/core/handlers/build-transaction-handler.ts
@@ -1,6 +1,5 @@
 import { TransactionHandler } from '../interface/transaction-handler'
 import { NormalizeDescriptionHandler } from './normalize-description-handler'
-import { OpenAICategorizationHandler } from './openai-categorization-handler'
 import { PersistTransactionHandler } from './persist-transaction-handler'
 import { PrismaUserRepository } from '@/infra/repositories/prisma/prisma-user-repository'
 import { PrismaTransactionRepository } from '@/infra/repositories/prisma/prisma-transaction-repository'
@@ -8,14 +7,13 @@ import { PrismaImportJobRepository } from '@/infra/repositories/prisma/prisma-im
 
 export function buildTransactionHandlerChain(): TransactionHandler {
     const normalize = new NormalizeDescriptionHandler()
-    const categorize = new OpenAICategorizationHandler()
     const persist = new PersistTransactionHandler(
         new PrismaUserRepository(),
         new PrismaTransactionRepository(),
         new PrismaImportJobRepository(),
     )
 
-    normalize.setNext(categorize).setNext(persist)
+    normalize.setNext(persist)
 
     return normalize
 }
